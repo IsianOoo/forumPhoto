ec8b3fd20bf281da1f36780eb223097ee7b84d10
Krystian Mączeński <maczenski.krystian@gmail.com>
2024-10-24 22:51:54 +0200

    added tailwindcss

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom"
 export default function Navbar() {
   return (
-    <nav>
+    <nav className="nav">
+      <a href="/" className="nav__title">Forum Photo</a>
+      
         <Link to='/'>Home</Link>
         <Link to='/register'>Register</Link>
         <Link to='/login'>Login</Link>
