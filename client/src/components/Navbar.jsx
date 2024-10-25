9dc3c8f24f62ea027742063ce9f6840d4b3c5828
KrystianMączeński <91785838+IsianOoo@users.noreply.github.com>
2024-10-25 18:04:22 +0200

    Navbar (#2)
    * added tailwindcss
    
    * Added navbar style

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,36 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 export default function Navbar() {
-  return (
-    <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/register'>Register</Link>
-        <Link to='/login'>Login</Link>
-    </nav>
-  )
+	return (
+		<nav className='bg-gray-500'>
+			<div className='max-w-7xl mx-auto px-3'>
+				<div className='flex items-center justify-between h-14'>
+          <div className='shrink-0'>
+					<a href='/' class='text-white text-xl font-bold'>
+						Forum Photo
+					</a>
+          </div>
+
+					<ul className='flex space-x-7'>
+						<li>
+							<a href='/' className='text-gray-300 hover:text-white hover:border-b-2 py-1 border-transparent transition-all duration-100  hover:border-white'>Home</a>
+						</li>
+						<li>
+							<a href='#' className='text-gray-300 hover:text-white hover:border-b-2 py-1 border-transparent transition-all duration-100  hover:border-white'>Photos</a>
+						</li>
+						<li>
+							<a href='#' className='text-gray-300 hover:text-white hover:border-b-2 py-1 border-transparent transition-all duration-100  hover:border-white'>Courses</a>
+						</li>
+						<li>
+							<a href='#' className='text-gray-300 hover:text-white hover:border-b-2 py-1 border-transparent transition-all duration-100  hover:border-white'>Competition</a>
+						</li>
+					</ul>
+
+					<div className='flex space-x-4'>
+						<a href='/register' className='text-gray-300 hover:text-white hover:border-b-2 py-1 border-transparent transition-all duration-100  hover:border-white font-bold'>Sign Up</a>
+						<a href='/login 'className='text-gray-300 hover:text-white hover:border-b-2 py-1 border-transparent transition-all duration-100  hover:border-white'>Log in</a>
+					</div>
+				</div>
+			</div>
+		</nav>
+	)
 }
