75a5b33332acababbba31de54b99a33fd1286dcf
KrystianMączeński <91785838+IsianOoo@users.noreply.github.com>
2024-11-07 21:09:14 +0100

    create design login page (#5)

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,43 +1,69 @@
 import React from 'react'
 import { useState } from 'react'
 import axios from 'axios'
-import {toast} from 'react-hot-toast'
+import { toast } from 'react-hot-toast'
 import { useNavigate } from 'react-router-dom'
 
 export default function Login() {
 	const navigate = useNavigate()
-    const [data,setData] = useState({
-        email:'',
-        password:'',
-    })
+	const [data, setData] = useState({
+		email: '',
+		password: '',
+	})
 
 	const loginUser = async (e) => {
 		e.preventDefault()
-		const {email,password} = data
+		const { email, password } = data
 		try {
-			const {data} = await axios.post('/auth/login',{
+			const { data } = await axios.post('/auth/login', {
 				email,
-				password
+				password,
 			})
 
-			if(data.error){
+			if (data.error) {
 				toast.error(data.error)
-			}else{
+			} else {
 				setData({})
 				navigate('/')
 			}
-		} catch (error) {
-			
-		}
+		} catch (error) {}
 	}
 	return (
-		<div>
-			<form onSubmit={loginUser}>
-				<label>Email</label>
-				<input type='email' placeholder='Wpisz email...' value={data.email}  onChange={(e)=>setData({...data,email:e.target.value})}/>
-				<label>Hasło</label>
-				<input type='password' placeholder='Wpisz hasło...' value={data.password} onChange={(e)=>setData({...data,password:e.target.value})} />
-				<button type='submit'>Loguj</button>
+		<div className='flex items-center justify-center min-h-screen bg-gray-100'>
+			<form onSubmit={loginUser} className='p-8 bg-white shadow-lg w-full max-w-sm'>
+				<h2 className='text-2xl font-bold text-pink-400 mb-6'>Log in</h2>
+				<div className='mb-4'>
+					<label className='text-sm mb-2 font-semibold text-gray-700 block'>Email</label>
+					<input
+						type='email'
+						placeholder='Wpisz email...'
+						value={data.email}
+						onChange={(e) => setData({ ...data, email: e.target.value })}
+						className='w-full px-4 py-2 text-gray-700 border rounded-md focus:outline-none focus:ring-2 focus:ring-gray-700'
+					/>
+				</div>
+				<div className='mb-6'>
+					<label className='text-gray-700 text-sm block font-semibold mb-2'>Password</label>
+					<input
+						type='password'
+						placeholder='Wpisz hasło...'
+						value={data.password}
+						onChange={(e) => setData({ ...data, password: e.target.value })}
+						className='text-gray-700 border rounded-md w-full px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-700'
+					/>
+				</div>
+
+				<button
+					type='submit '
+					className='bg-pink-400 text-white font-semibold rounded-md w-full py-2 px-4 focus:outline-none focus:ring-2 focus:ring-gray-700'>
+					Loguj
+				</button>
+				<p className='text-sm text-gray-500 mt-4 text-center'>
+					Don't have an account?{' '}
+					<a href='#' className='text-pink-400 hover:underline'>
+						Sign up
+					</a>
+				</p>
 			</form>
 		</div>
 	)
