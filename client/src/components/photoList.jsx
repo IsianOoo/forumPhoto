0b85e52f8519749dfea9a6106271e24c613ce4d8
Krystian Mączeński <maczenski.krystian@gmail.com>
2025-02-09 17:04:49 +0100

    create likes and comments

--- client/src/components/photoList.jsx
+++ client/src/components/photoList.jsx
@@ -1,39 +1,41 @@
-import axios from 'axios';
-import { toast } from 'react-hot-toast';
-import CardPhoto from "./cardPhoto";
-import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+import { toast } from 'react-hot-toast'
+import CardPhoto from './cardPhoto'
+import React, { useEffect, useState } from 'react'
 
 export default function PhotoList() {
-    const [photos, setPhotos] = useState([]);
+	const [photos, setPhotos] = useState([])
 
-    useEffect(() => {
-        const fetchPhotos = async () => {
-            try {
-                toast('Fetching photos...');
-                const response = await axios.get('http://localhost:8000/photo');
-                
-                setPhotos(response.data);
-                
-                toast.success('Photos loaded successfully!');
-            } catch (err) {
-                
-                toast.error(`Error: ${err.message}`);
-            }
-        };
+	useEffect(() => {
+		const fetchPhotos = async () => {
+			try {
+				toast('Fetching photos...')
+				const response = await axios.get('http://localhost:8000/photo')
 
-        fetchPhotos();
-    }, []);
+				setPhotos(response.data)
 
-    return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
-            {photos.map(photo => (
-                <CardPhoto
-                    key={photo._id}
-                    title={photo.title}
-                    description={photo.description}
-                    imageUrl={photo.imageUrl}
-                />
-            ))}
-        </div>
-    );
+				toast.success('Photos loaded successfully!')
+			} catch (err) {
+				toast.error(`Error: ${err.message}`)
+			}
+		}
+
+		fetchPhotos()
+	}, [])
+
+	return (
+		<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4'>
+			{photos.map((photo) => (
+				<CardPhoto
+					key={photo._id}
+                    id={photo._id}
+					title={photo.title}
+					description={photo.description}
+					imageUrl={photo.imageUrl}
+					initialLikes={photo.likes}
+					initialComments={photo.comments}
+				/>
+			))}
+		</div>
+	)
 }
