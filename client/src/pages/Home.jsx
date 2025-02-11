ed86f0813f016027db9c8439a99576bcfbc1d199
Krystian Mączeński <maczenski.krystian@gmail.com>
2025-02-11 14:59:42 +0100

    add animation on home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <div className='relative w-full h-screen bg-cover bg-center bg-no-repeat' style={{ backgroundImage: `url('../img/homebcg.jpg')` }}>
       <div className='  absolute bg-black bg-opacity-30 w-screen h-screen'></div>
-      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center px-4 '>
+      <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center px-4'>
         
-        <h1 className='uppercase font-bold text-4xl md:text-5xl lg:text-7xl text-white'>Share your vision of the world!</h1>
-        <p className='text-white text-sm md:text-base lg:text-lg mt-4 max-w-xl mx-auto'>Discover captivating photos from all styles and genres worldwide. Here, true artisty takes center stage, not fleeting trends.</p>
+        <h1 className='uppercase font-bold text-4xl md:text-5xl lg:text-7xl text-white opacity-0 animate-fadeInSlide'>Share your vision of the world!</h1>
+        <p className='text-white text-sm md:text-base lg:text-lg mt-4 max-w-xl mx-auto opacity-0 animate-fadeInSlide delay-300'>Discover captivating photos from all styles and genres worldwide. Here, true artisty takes center stage, not fleeting trends.</p>
       </div>
     </div>
   )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			animation: {
+				fadeInSlide: 'fadeInSlide 1s ease-out forwards',
+			},
+			keyframes: {
+				fadeInSlide: {
+					'0%': { opacity: 0, transform: 'translateY(-20px)' },
+					'100%': { opacity: 1, transform: 'translateY(0)' },
+				},
+			},
+		},
+	},
+	plugins: [],
 }
-
